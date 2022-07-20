d88c44023a986ad2d07fb4770d868c7b5c30ccf5
Sam Allen <samallen659@gmail.com>
2022-07-20 22:57:44 +0100

    Setup theme for tailwindcss and daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [],
+	content: ["./public/**/*.{html,js}"],
 	theme: {
 		extend: {},
+		colors: {
+			purple: "#755CDE",
+			orange: "#F6A560",
+			pink: "#F39E9E",
+			red: "#EB7565",
+			cyan: "#61C4B7",
+			maroon: "#552049",
+			black: "#030303",
+			grey: "7A746E",
+			white: "#FFF7F0",
+		},
+		fontFamily: {
+			sans: ["Plus Jakarta Sans", "sans-serif"],
+		},
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#030303",
+					"primary-focus": "#755CDE",
+					"primary-content": "#FFF7F0",
+					secondary: "#EB7565",
+					"secondary-focus": "#F6A560",
+					"secondary-content": "#FFF7F0",
+					"base-100": "#FFF7F0",
+				},
+			},
+		],
 	},
 	plugins: [require("daisyui")],
 };
